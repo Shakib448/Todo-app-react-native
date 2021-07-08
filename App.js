0b880fce9b7823b45bc6e0248382f32edd24fbea
Shakiba448 <shakiba448@gmail.com>
2021-07-08 08:58:36 +0600

    React native

--- App.js
+++ App.js
@@ -1,29 +1,33 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {Text, useColorScheme, View} from 'react-native';
+import {Text, View, Image, StyleSheet} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+const App = () => {
   return (
-    <View>
-      <Text>Hello world</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello world</Text>
+      <Image
+        source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+        style={styles.img}
+      />
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'blue',
+    fontSize: 30,
+  },
+  img: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+  },
+});
