17c0bf5669b815c27997017c02f29e0923863ab5
Shakiba448 <shakiba448@gmail.com>
2021-07-08 10:05:59 +0600

    React native

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,12 +1,13 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Icon } from 'react-native';
 
 const ListItem = ({ item }) => {
   return (
     <TouchableOpacity style={styles.listItem}>
       <View style={styles.listItemView}>
         <Text style={styles.listItemText}>{item.text}</Text>
+        <Icon name="remove" size={20} color="firebrick" />
       </View>
     </TouchableOpacity>
   );
@@ -15,5 +16,18 @@ const ListItem = ({ item }) => {
 export default ListItem;
 
 const styles = StyleSheet.create({
-  listItem: {},
+  listItem: {
+    padding: 15,
+    backgroundColor: '#f8f8f8',
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+  },
+  listItemView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  listItemText: {
+    fontSize: 18,
+  },
 });
